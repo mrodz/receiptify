4c21bb3b1388a138a8b9a20236dd06024016767a
mrodz <79176075+mrodz@users.noreply.github.com>
2024-08-10 12:31:07 -0700

    Overhaul frontend

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,29 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from "react-router-dom"
-import reportWebVitals from './reportWebVitals';
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import App from './App.tsx'
+import './index.css'
+import Login from './pages/login/Login.tsx'
+import PasswordReset from './pages/password_reset/PasswordReset.tsx'
 
-import './index.css';
-import App from './App';
-import Login from './pages/login/Login';
-import PasswordReset from './pages/password_reset/PasswordReset';
-
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<App/>}></Route>
-        <Route path="/login" element={<Login/>}></Route>
-        <Route path="/login/reset" element={<PasswordReset/>}></Route>
+        <Route path="/" element={<App />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/login/reset" element={<PasswordReset />}></Route>
       </Routes>
     </BrowserRouter>
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  </StrictMode>,
+)
--- frontend/src/pages/login/Login.tsx
+++ frontend/src/pages/login/Login.tsx
@@ -1,26 +1,50 @@
-import React from 'react';
 import { useNavigate } from "react-router-dom";
+import { TextField, Button, Link, Card, FormControl, Typography } from "@mui/material"
 
-import { TextField, Button, Link } from "@mui/material"
 import "./Login.css"
+import google from '../../assets/google.svg'
+import github from '../../assets/github.svg'
 
 function Login() {
   const navigate = useNavigate()
-  
+
   return (
-    <div className="login-container">
-        <Button>Log in with Google</Button>
-        <Button>Log in with GitHub</Button>
+    <main className="flex items-center justify-center h-full">
+      <Card className="p-8 w-6/7 sm:w-2/3 md:w-1/2 max-w-4xl">
+        <Typography className="text-center" variant="h3" component="h1" gutterBottom>
+          <span className="max-sm:hidden">
+            Sign in to&nbsp;
+          </span>
+          Receiptify
+        </Typography>
 
-        <TextField className="login-field" label="Username" type="text"></TextField>
-        <div className="password-container">
-            <TextField className="login-field" label="Password" type="password"></TextField>
-            <Button onClick={() => navigate("/login/reset")}>Forgot your password?</Button>
-        </div>
+        <FormControl className="w-full flex flex-col items-center gap-4">
+          <div className="w-4/5 sm:w-2/5 md:w-3/5">
+            <TextField className="w-full" label="Username" type="text"></TextField>
 
-        <Button>Log in</Button>
-        <Link href="/signin">Don't have an account? Sign up.</Link>
-    </div>
+            <div className="flex flex-col mt-4">
+              <TextField className="w-full" label="Password" type="password"></TextField>
+              <Button onClick={() => navigate("/login/reset")}>Forgot password?</Button>
+            </div>
+          </div>
+
+          <div className="grid max-lg:grid-rows-2 lg:grid-cols-2 w-4/5 sm:w-2/5 md:w-3/5">
+            <Button variant="outlined" className="grid grid-cols-[1fr_auto] gap-3">
+              <img className="w-8" src={google} />
+              Sign in with Google
+            </Button>
+            <Button variant="outlined" className="grid grid-cols-[1fr_auto] gap-3">
+              <img className="w-8" src={github} />
+              Sign in in with GitHub
+            </Button>
+          </div>
+
+          <Button variant="contained">Log in</Button>
+
+          <Link href="/signup">Don't have an account? Sign up.</Link>
+        </FormControl>
+      </Card>
+    </main>
   );
 }
 
