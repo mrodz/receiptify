4bba8fe8185b88afdb4fa6eb717665ca63b64b5a
Zander Baker <zanderbaker100@gmail.com>
2024-08-09 22:33:37 -0700

    Create login page

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,14 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import reportWebVitals from './reportWebVitals';
+
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Login from './pages/login/Login';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App/>}></Route>
+        <Route path="/login" element={<Login/>}></Route>
+      </Routes>
+    </BrowserRouter>
     <App />
   </React.StrictMode>
 );
