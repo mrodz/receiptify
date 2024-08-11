6b5331d3da78aad732d5c2cbc1336f0a4957296b
mrodz <79176075+mrodz@users.noreply.github.com>
2024-08-10 19:17:21 -0700

    Add firebase data

---
+++ backend/functions/src/index.ts
@@ -0,0 +1,6 @@
+// import * as https from "firebase-functions/v2/https";
+// import * as logger from "firebase-functions/logger";
+
+// Start writing functions
+// https://firebase.google.com/docs/functions/typescript
+
